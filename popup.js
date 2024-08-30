d81f473f3b411a1c09f5bfaebe196585e3258b8e
Joel Prince <63162849+joelprince2601@users.noreply.github.com>
2024-08-30 20:39:23 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,15 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Add event listener to 'Start Drawing' button
-  document.getElementById('startDrawingBtn').addEventListener('click', function() {
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, { action: 'startDrawing' });
-      });
-  });
+    // Add event listener to 'Start Drawing' button
+    document.getElementById('startDrawingBtn').addEventListener('click', function() {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.scripting.executeScript({
+              target: { tabId: tabs[0].id },
+              files: ['html2canvas.min.js', 'drawingTool.js']
+            });
+        });
+    });
 
-  // Add event listener to 'Collect News' button
-  document.getElementById('collectNewsButton').addEventListener('click', function() {
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, { action: 'fetchHeadlines' });
-      });
-  });
+    // Add event listener to 'Collect News' button
+    document.getElementById('collectNewsBtn').addEventListener('click', function() {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, { action: 'fetchHeadlines' });
+        });
+    });
 });
